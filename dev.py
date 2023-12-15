706360c15bbb35630dfe571970720d547f6d98d5
Avdiienko Illia <avil48job@gmail.com>
2023-12-15 15:36:41 +0200

    fix(dev-env): enable debug logging

--- dev.py
+++ dev.py
@@ -1,7 +1,11 @@
+from logging import basicConfig as BasicLogging
+from logging import DEBUG
+
 from aiohttp.web import run_app
 
 from configs import DEV_HOST
 from main import app
 
 if __name__ == '__main__':
-    run_app(app, host=DEV_HOST)
+    BasicLogging(level=DEBUG)
+    run_app(app, host=DEV_HOST, port=8080)

372896562e870f3e8e5b1fe8a602e0d7ca93882c
Avdiienko Illia <avil48job@gmail.com>
2023-12-15 19:48:13 +0200

    feat(choose-your-haiku): enhance frontend so it supports selection one of multiple haikus

--- static/scripts.js
+++ static/scripts.js
@@ -1,39 +1,86 @@
 function insertText(textToInsert) {
-  document.getElementById('textInput').value = textToInsert;
+    document.getElementById("textInput").value = textToInsert;
 }
 
-async function sendText() {
-  const textInput = document.getElementById('textInput').value;
+function generateAPIcallId(length = 6) {
+    let result = "";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
 
-  if (textInput) {
-    const response = await fetch('http://127.0.0.1:8080/haikufy', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({text: textInput}),
-    });
+async function callHaikufyRemoteProcedure(haiku_id, textInput) {
+    return fetch(
+        "http://127.0.0.1:8080/rpc",
+        {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(
+                {
+                    jsonrpc: "2.0",
+                    id: haiku_id,
+                    method: "haikufy",
+                    params: [textInput],
+                }
+            ),
+        }
+    );
+}
 
-    const data = await response.json();
-    // Create a new paragraph element for each result and append it to the result div
-    const haiku = document.createElement("section", );
+function buildHaikuUI(haiku_id, data) {
+    const haiku = document.createElement("section");
+    haiku.id = haiku_id
 
     const title = document.createElement("h2");
     title.innerText = data.haikuTitle;
     title.title = data.userPrompt;
 
-    const text = document.createElement('p');
-    text.innerText = data.processedText;
-
-    const breakLineAfter = document.createElement("hr")
-
     haiku.appendChild(title);
-    haiku.appendChild(text);
-    haiku.appendChild(breakLineAfter);
 
-    document.getElementById('result').appendChild(haiku);
+    // create HTMLElements for each haiku variant
+    const haikuVariants = data.haikuVariants.map(
+        (haikuVariantFromResponse) => {
+            const haikuVariant = document.createElement("button");
+            haikuVariant.innerText = haikuVariantFromResponse.text;
+            haikuVariant.id = haikuVariantFromResponse.id;
+            haikuVariant.className = "haiku-variant";
+            haikuVariant.addEventListener(
+                "click",
+                () => alert(haikuVariantFromResponse.id)
+            );
+            return haikuVariant;
+        }
+    );
 
-    // Clear the textarea for the next input
-    document.getElementById('textInput').value = '';
-  }
+    const haikuVariantsContainer = document.createElement("section");
+    haikuVariantsContainer.className = "haiku-variants-container";
+    haikuVariants.map(variant => haikuVariantsContainer.appendChild(variant));
+
+    haiku.appendChild(haikuVariantsContainer);
+    haiku.appendChild(document.createElement("hr"))
+
+    return haiku
+}
+
+
+async function sendText() {
+    const textInput = document.getElementById("textInput").value;
+
+    if (textInput) {
+        const haiku_id = generateAPIcallId();
+        const response = await callHaikufyRemoteProcedure(haiku_id, textInput);
+
+        const data = await response.json();
+
+        const haiku = buildHaikuUI(haiku_id, data)
+
+        document.getElementById("userHaikus").appendChild(haiku);
+
+        document.getElementById("textInput").value = "";
+    }
 }
